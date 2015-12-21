8990fb5fc7b9cd5c2355880b0b3cf52906ec37a2
x2431915 <x2413915@gmail.com>
2015-12-21 18:09:40 +0800

    abstract class Questions,class Question extends Questions

--- src/Question.java
+++ src/Question.java
@@ -5,38 +5,12 @@ abstract class Questions {
 	abstract void print();
 }
 
-public class Question extends Questions {
+class Question extends Questions {
 	String description;
 	public Question(String desc) {
 		this.description = desc;
 	}
 	public void print() {
 		System.out.println(description);
-	}
-}
-
-public class Paper extends Questions {
-	private ArrayList<Questions> list ;
-	Paper(){
-		 list = new ArrayList<Questions>();
-	}
-	public void add( Questions questions ) {
-		list.add(questions);
-	}
-	
-	public void print() {
-		Iterator<Questions> iterator = list.iterator();
-		while( iterator.hasNext() ){
-			Questions c = iterator.next();
-			c.print();
-		}
-	}
-}
-
-class Main {
-	void m1( Questions[] c ){
-		for( Questions cc : c){
-			cc.print();
-		}
 	}
 }
